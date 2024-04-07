c76eae5d88eb5aa0cd9098224ae2d933394c05c7
Archana-tamil <96418152+Archana-tamil@users.noreply.github.com>
2024-04-07 11:52:21 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,61 @@
+import express from "express";
+import {Server} from "socket.io";
+import cors from "cors";
+import http from "http";
+import { connect } from "./config.js";
+import { chatModel } from "./chat.schema.js";
+
+
+
+
+const app=express();
+
+//creating a server using http
+const server=http.createServer(app);
+
+//creating a socket server
+const io=new Server(server,{
+    cors:{
+        origin:"*",
+        methods:["GET","POST"]
+    }
+});
+
+//using socket events
+io.on('connection',(socket)=>{
+    console.log("connection is established");
+    socket.on("join",(data)=>{
+        socket.username=data;
+
+        //send old messages to client
+
+    chatModel.find().sort({timestamp:1}).limit(50)
+    .then(messages=>{
+        socket.emit("load_message",messages);
+    }).catch(err=>{
+        console.log(err);
+    })
+    })
+    socket.on("new-message",(message)=>{
+        let userMessage={
+            username:socket.username,
+            message:message
+        }
+        const newChat=new chatModel({
+            username:socket.username,
+            message:message,
+            timestamp:new Date()
+        });
+        newChat.save();
+        socket.broadcast.emit("broadcast_message",userMessage);
+    })
+    socket.on("disconnect",()=>{
+        console.log("connection is disconnected");
+    });
+});
+
+
+server.listen(3000,()=>{
+    console.log("App is listening on the port 3000");
+    connect();
+});
